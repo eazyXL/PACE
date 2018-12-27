2730cfaa8e10eae41cde8b90dd9298cceb4a7dce
eazyxl <elanou12@gmail.com>
2018-12-27 01:55:44 -0500

    updated block generation algorithm

--- opto.py
+++ opto.py
@@ -1,3 +1,4 @@
+import time
 import sys
 import pygame
 
@@ -13,6 +14,7 @@ class rectObj:
 screen_color = (255, 255, 255)
 screen = pygame.display.set_mode((width, height))
 screen.fill(screen_color)
+pygame.display.set_caption('Optokinetic Tape Test')
 pygame.display.flip()
 
 
@@ -20,17 +22,30 @@ pygame.display.flip()
 numBlocks = int(width/100)+2
 blockList = []
 
+count = 0
+toggle = 0
+# 0 -> red
+# 1 -> black
 for x in range(numBlocks):
-		tempBlock = rectObj()
-		if x == 0:
-			tempBlock.xCoord = -100
-		else:
-			if x%2 == 1:
-				tempBlock.color = black
-				tempBlock.xCoord = 100*(x-1)
-			else:
-				tempBlock.xCoord = 100*(x-1)
-		blockList.append(tempBlock)
+    tempBlock = rectObj()
+
+    if toggle == 0 :
+        tempBlock.xCoord = x*100
+        toggle = 1
+        blockList.append(tempBlock)
+        continue
+
+    if toggle == 1 :
+        tempBlock.xCoord = x*100
+        tempBlock.color = black
+        count += 1
+        blockList.append(tempBlock)
+        if count == 2 :
+            count = 0
+            toggle = 0
+
+for blocks in blockList:
+    blocks.xCoord -= 100
 
 
 #for block in blockList:
@@ -51,6 +66,6 @@ while running:
 		block.xCoord += speed
 		if block.xCoord == 1100:
 			block.xCoord = -100
-
-				
+        #sleep needed to fine tune speed depending on system
+	time.sleep(.005)			
 	pygame.display.update()
