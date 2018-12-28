4a0cdf60274adec3e82fe4e65aeddfa4529152d3
eazyxl <elanou12@gmail.com>
2018-12-27 19:07:10 -0500

    need to fix coords

--- opto.py
+++ opto.py
@@ -59,11 +59,12 @@ surf.fill((255,255,255))
 pygame.draw.rect(surf, green, (0, 0, 300, 150), 7)
 screen.blit(surf, (350, 75))
 
+blockFile = open("blockPos.txt", "w")
+eyeFile = open("eyePos.txt", "w")
+
 
 running = True
 speed = 5
-
-
 runs = 0
 forward = 1
 while running:
@@ -81,17 +82,30 @@ while running:
 			time.sleep(.5)
 			speed = speed*(-1)
 
-
+#want to track when block in in xCoords 350 - 650
 	for block in blockList:
 		pygame.draw.rect(screen, block.color, (block.xCoord, 100, 100, 100))
+
+
+		if block.xCoord > 250 and block.xCoord < 650:
+			blockFile.write("Block in Focus Zone at Coords:\n")
+			blockFile.write("%i %s %i\n" % (block.xCoord, "to", (block.xCoord+100)))	
+													
+
 		block.xCoord += speed
 		if block.xCoord == 1100:
 			block.xCoord = -100
 		if block.xCoord == -200:
 			block.xCoord = 1000
+		print(block.xCoord)
+													
         #sleep needed to fine tune speed depending on system
 	time.sleep(.005)			
 	pygame.display.update()
+
+
+
+
 	v,f = pygame.mouse.get_pos()
-	print(v,f)
+	#print(v,f)
 	runs += 1
