06505ba42944aa997b56c92cdb5379e95e13108d
unknown <elanou12@gmail.com>
2019-04-15 15:38:14 -0700

    fixing values

--- opto2560.py
+++ opto2560.py
@@ -11,8 +11,10 @@ green = (0,255,0)
 class rectObj:
 	xCoord = 0
 	color = red
+#center the window
 os.environ['SDL_VIDEO_CENTERED'] = '1'
-(width, height) = (2500, 300)
+#window size
+(width, height) = (2560, 300)
 screen_color = (255, 255, 255)
 screen = pygame.display.set_mode((width, height))
 screen.fill(screen_color)
@@ -20,8 +22,8 @@ pygame.display.set_caption('Optokinetic Tape Test')
 pygame.display.flip()
 
 
-
-numBlocks = int(width/100)+5
+#create 29 blocks to add to the list
+numBlocks = int((width-60)/100)+5
 blockList = []
 
 count = 0
@@ -42,20 +44,22 @@ for x in range(numBlocks):
         tempBlock.color = black
         count += 1
         blockList.append(tempBlock)
+        #create 4 black blocks for every 1 red
         if count == 4 :
             count = 0
             toggle = 0
 
+#debugging for list size
 print("LENGTH")
 print(len(blockList))
-
 for x in range(len(blockList)):
 	print(x, " ", blockList[x].xCoord, blockList[x].color)
-#debugging for list size
 
+#create surface to create green rectangle
 surf = pygame.Surface((400,150))
 surf.fill((255,255,255))
 pygame.draw.rect(surf, green, (0, 0, 400, 150), 7)
+#place at this pixel location
 screen.blit(surf, (1080, 75))
 
 blockFile = open("blockPos.txt", "w")
@@ -86,8 +90,9 @@ while running:
 	for block in blockList:
 		pygame.draw.rect(screen, block.color, (block.xCoord, 100, 100, 100))
 
-		if block.xCoord > 250 and block.xCoord < 650 and block.color == red:
-			tempBlock = block
+		#if block visible inside region
+		if block.xCoord > 980 and block.xCoord < 1480 and block.color == red:
+			tempBlock.xCoord = block.xCoord
 
 		block.xCoord += speed
 		if block.xCoord == 2900:
@@ -104,11 +109,12 @@ while running:
 
 
 	#blockFile.write("Block in Focus Zone at Coords:\n")
-	if tempBlock.xCoord  < 350:
-		blockFile.write("%i%s%i %s %i\n" % (350, ",", (tempBlock.xCoord+100), "at time:", updateTime))
-	elif tempBlock.xCoord > 550:
-		blockFile.write("%i%s%i %s %i\n" % (tempBlock.xCoord, ",", 650, "at time:", updateTime))
-	else:
+	#block can be partially in the region from the left, fully enclosed or partially in from the right side
+	if tempBlock.xCoord  < 1080 and tempBlock.xCoord  > 980:
+		blockFile.write("%i%s%i %s %i\n" % (1080, ",", (tempBlock.xCoord+100), "at time:", updateTime))
+	elif tempBlock.xCoord > 1380 and tempBlock.xCoord  < 1480:
+		blockFile.write("%i%s%i %s %i\n" % (tempBlock.xCoord, ",", 1480, "at time:", updateTime))
+	elif tempBlock.xCoord  < 1480 and tempBlock.xCoord  >= 1080:
 		blockFile.write("%i%s%i %s %i\n" % (tempBlock.xCoord, ",", (tempBlock.xCoord+100), "at time:", updateTime))
 
 	eyeFile.write("%i%s%i %s %i\n" % (mouseX, ",", mouseY, "at time:", updateTime))
